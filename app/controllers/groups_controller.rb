f9707b5145f9fe4ebbae0b30d7207271601a13d6
shuibo <sbjyemail@gmail.com>
2017-04-21 11:26:12 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,6 +19,7 @@ before_action :authenticate_user! , only: [:new]
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
 
     if@group.save
        redirect_to groups_path
