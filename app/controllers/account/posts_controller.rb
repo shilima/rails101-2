53f6fd23ebd8c89c3fe17a5805ebaf554472c6bf
shuibo <sbjyemail@gmail.com>
2017-04-22 22:15:39 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,14 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: 'Groups Deleted'
+  end
 end
