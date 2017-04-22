0129114c89ce944f0891e1ecb089dc4dfa47a514
shuibo <sbjyemail@gmail.com>
2017-04-22 20:40:37 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
 before_action :find_group_and_check_permission, only: [:edit, :update,:destroy]
 
   def index
@@ -44,6 +44,32 @@ before_action :find_group_and_check_permission, only: [:edit, :update,:destroy]
       redirect_to groups_path, alert: 'Groups Delete'
     end
 
+  def join
+    @group = Group.find(params[:id])
+
+     if !current_user.is_member_of?(@group)
+       current_user.join!(@group)
+       flash[:notice] = "加入本讨论版成功!"
+     else
+       flash[:warning] = "你已经是本讨论版成员了！"
+     end
+
+     redirect_to group_path(@group)
+   end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论组!"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+       redirect_to group_path(@group)
+     end
+
     private
 
     def find_group_and_check_permission
@@ -56,5 +82,4 @@ before_action :find_group_and_check_permission, only: [:edit, :update,:destroy]
     def group_params
       params.require(:group).permit(:title, :description)
     end
-
-end
+   end
